ce14b4b61ba687f3a92bd1b081598175ae00dd6c
Hafiz Aziz <hafiz0x01@gmail.com>
2022-04-23 15:14:59 +0100

    JS Code Injection Update

---
+++ app/src/main/java/app/beetlebug/ctf/DisplayXSS.java
@@ -0,0 +1,14 @@
+package app.beetlebug.ctf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayXSS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_xss);
+    }
+}
